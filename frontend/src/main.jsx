a9e2176122dae83af5694619c1b1f2f62dc0673b
tejas git <d9489079@gmail.com>
2025-05-31 20:39:23 +0530

    added onclick property on more button to get remaining doctors details and navigate to doctors tab page

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,10 +2,13 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
+import AppContextProvider from "./context/AppContext.jsx";
 import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
-    <App />
+    <AppContextProvider>
+      <App />
+    </AppContextProvider>
   </BrowserRouter>
 );
